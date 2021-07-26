5a560d8f569c1b5b51c72c03ac1c84963de3c8f1
Jim <jadavey@hotmail.com>
2021-07-26 11:55:59 +0100

    Delete by ID
    Works via insomnia

--- routes/DeleteRecipeRouter.js
+++ routes/DeleteRecipeRouter.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const Recipe = require("../models/Recipe");
 
-router.delete("/", (req, res) => {
+router.delete("/:recipeName", (req, res) => {
   Recipe.findOneAndDelete({ recipeName: req.params.recipeName }, (err, obj) => {
     if (err) {
         console.log(err);
